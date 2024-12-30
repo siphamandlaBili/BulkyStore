73ffae4bad07a5b715d50ef0ed6aa58cf9496845
Siphamandla <siphabili@gmail.com>
2024-12-30 21:14:33 +0200

    cartegories mvc

---
+++ BulkyBooks/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using BulkyBooks.Data;
+using BulkyBooks.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBooks.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<CategoryModel> objCategoryList = _db.categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ BulkyBooks/Models/CategoryModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace BulkyBooks.Models
+{
+    public class CategoryModel
+    {
+        public int Id { get; set; }
+        [DisplayName("Cartegory Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
