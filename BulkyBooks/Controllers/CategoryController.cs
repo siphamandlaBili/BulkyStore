6ec0dcc52b1f6e6064257adbdbbce0c94518c5e2
Siphamandla <siphabili@gmail.com>
2024-12-31 21:23:16 +0200

    edit functionality

--- BulkyBooks/Controllers/CategoryController.cs
+++ BulkyBooks/Controllers/CategoryController.cs
@@ -32,5 +32,59 @@ namespace BulkyBooks.Controllers
             }
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id <= 0)
+            {
+                return NotFound();
+            }
+            
+            CategoryModel singleCategoryToBeEdited = _db.categories.Find(id);
+
+            if (singleCategoryToBeEdited == null)
+            {
+                return NotFound();
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Edit(CategoryModel obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id <= 0)
+            {
+                return NotFound();
+            }
+
+            CategoryModel singleCategoryToBeEdited = _db.categories.Find(id);
+
+            if (singleCategoryToBeEdited == null)
+            {
+                return NotFound();
+            }
+            return View();
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeleteCartegory(int? id)
+        {
+            CategoryModel singleCategoryToBeDleted = _db.categories.Find(id);
+            _db.categories.Remove(singleCategoryToBeDleted);
+
+            _db.categories.Remove(singleCategoryToBeDleted);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+            
+        }
     }
 }
