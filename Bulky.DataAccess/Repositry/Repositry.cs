6ed3768a0bc98007a866e063d9e5c57937005db6
Siphamandla <siphabili@gmail.com>
2025-01-11 22:06:28 +0200

    Irepositry interface setup

---
+++ Bulky.DataAccess/Repositry/Repositry.cs
@@ -0,0 +1,47 @@
+﻿using Bulky.DataAccess.Repositry.Irepositry;
+using BulkyBooks.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repositry
+{
+    public class Repositry<T> : Irepositry<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbset;
+        public Repositry(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbset = _db.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            dbset.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
